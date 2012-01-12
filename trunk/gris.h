62358b3d6506f25288da8a94c991d8b8c409b88c
labed.sa@gmail.com <labed.sa@gmail.com@bc2f1554-a317-6368-4e16-c2750b02dd0f>
2012-01-12 14:42:47 +0000

    Gris Ok

---
+++ trunk/gris.h
@@ -0,0 +1,17 @@
+#ifndef GRIS_H
+#define GRIS_H
+#include <QImage>
+#include "Image.h"
+
+class Gris
+{
+public:
+   Gris(int ml, int mc);
+   void calNivGris(RgbImage image);
+protected:
+private:
+    int maxlig;
+    int maxcol;
+};
+
+#endif // GRIS_H
